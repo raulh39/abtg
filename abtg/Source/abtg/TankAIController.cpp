be87c69878b44e50ae4294dbc92c1206890ca164
raul.huertas <raulh39@gmail.com>
2017-06-25 12:46:15 +0200

    Added a TankAIController and logged what Tank it controls

---
+++ abtg/Source/abtg/TankAIController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	auto myPawn = GetControlledTank();
+	if (myPawn) {
+		UE_LOG(LogTemp, Warning, TEXT("ATankAIController controlling %s"), *myPawn->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("ATankAIController is not controlling any ATank"));
+	}
+}
